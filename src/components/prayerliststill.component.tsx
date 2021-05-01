58e23460c97dac14b753e0f1c6500023fa6b911a
Aziz <shaqhuseynov@gmail.com>
2021-05-01 19:01:36 +0200

    switched to css
    sass gives error after last node update, so i switched to css for now

--- src/components/prayerliststill.component.tsx
+++ src/components/prayerliststill.component.tsx
@@ -1,27 +1,27 @@
 import React from "react";
 import { TPrayerList } from "../assist/types";
 
-import Prayer from "./prayer.component";
+import { MdBrightness7 } from "react-icons/md";
 
 const PrayerListStill = ({ prayers }: TPrayerList) => (
   <div className="row" id="times">
     {prayers.map((prayer, index) => {
-      const classes = [
-        "col-sm-12",
-        "col-md-4",
-        "alert",
-        "alert-light",
-        "col-lg-2",
-        "text-muted",
-      ];
       return (
-        <Prayer
-          classes={classes.join(" ")}
-          prayer={prayer}
-          current={false}
-          index={index}
-          key={index}
-        />
+        <div className="col-sm-12 col-md-4 alert alert-light text-muted">
+          <div className="row">
+            <h6 className="col-7 col-md-12 align-self-center">{prayer.title}</h6>
+
+            <div className="col-5 col-md-12">
+              <h4>{prayer.time}</h4>
+
+              {index === 1 ? (
+                  <MdBrightness7  />
+                ) : (
+                 "" 
+                )}
+            </div>
+          </div>
+        </div>
       );
     })}
   </div>
